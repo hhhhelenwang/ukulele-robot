65ed207b4d3c119f0b50cb17da651cfc5b7696f6
jw795 <jw795@cornell.edu>
2023-05-09 21:41:11 -0400

    init

---
+++ src/ukulele-robot.ino
@@ -0,0 +1,21 @@
+// pins to stepping motors
+const int gMotorPin;
+const int cMotorPin;
+const int eMotorPin;
+const int aMotorPin;
+
+// pins to the blue motors
+
+
+// pin to high speed motor
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
